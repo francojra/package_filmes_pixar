4c4b95898939d38b4be16ce2e8bbd6d130d8a6e8
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-07-20 23:43:20 -0300

    Add files via upload

---
+++ package_dados_filmes_pixar.R
@@ -0,0 +1,25 @@
+# Base de dados - Curso R ------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 19/07/22 ---------------------------------------------------------------------------------------------------------------------------
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(dados)
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(tidyr)
+library(forcats)
+
+# Pacote - Filmes Pixar -----------------------------------------------------------------------------------------------------------------------------
+
+### O pacote pixarfilms apresenta algumas bases de dados com informações sobre 
+### filmes da Pixar. Todas as bases foram traduzidas e disponibilizadas no 
+### pacote dados!
+
+### Essas bases podem ser utilizadas para treinar manipulação e visualização 
+### de dados. Na minha opinião, essas bases são super interessantes para 
+### ensinar os concentos dos joins do dplyr!
+
+# Identificar bases disponíveis e carregar dados -------------------------------------------------------------------------------------------
+
